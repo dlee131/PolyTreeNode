acbb9ddf5292dc7638da21b38e88b51a1b21bccc
Daniel Lee <dlee13195@gmail.com>
2022-11-28 14:44:55 -0500

    initialize

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
+    
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(node)
+        @parent = node.parent
+        @parent.children << node
+        node.parent = @parent
+        @parent.children << node
+    end
 
 end
